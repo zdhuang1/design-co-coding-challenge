4948b151ace4aca7d879d397b25baeb8c23b4418
Zachary Huang <zdhuang@ucsd.edu>
2026-01-30 19:09:16 -0800

    Configure site for Github Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export", // tells Next.js to export a static site
   basePath: "/design-co-coding-challenge",
-  assetPrefix: "/design-co-coding-challenge/",
+  assetPrefix: "./",
 };
 
 export default nextConfig;
